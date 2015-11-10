81ffbdde29df7395439dd82a5165ac6d883f3938
monikabu <mna.burdzy@gmail.com>
2015-11-10 11:37:30 +0100

    Handle failure redirect

---
+++ app/models/devise/custom_failure.rb
@@ -0,0 +1,5 @@
+class Devise::CustomFailure < Devise::FailureApp
+  def redirect_url
+     root_url
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :omniauthable, :trackable
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      user = User.create(name: data["name"],
+         email: data["email"],
+         password: Devise.friendly_token[0,20]
+      )
+    end
+    user
+  end
+end
