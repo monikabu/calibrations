cf11cebec71e4a5c87e41edfeafb2caeec803701
monikabu <mna.burdzy@gmail.com>
2015-12-26 15:05:15 +0100

    Get valid params

--- app/controllers/slack_users_controller.rb
+++ app/controllers/slack_users_controller.rb
@@ -4,17 +4,20 @@ class SlackUsersController < ApplicationController
 
   def create
     SlackUser.create!(slack_user_params)
+    render text: "An integration request has been created"
   end
 
   private
 
   def slack_user_params
     attributes = params.permit(:token, :user_name, :text)
+    attributes[:slack_token] = attributes.delete(:token)
+    attributes[:name] = attributes.delete(:user_name)
     attributes[:user_id] = find_user_by_email(attributes.delete(:text)).id
     attributes
   end
 
   def find_user_by_email(email)
-    User.find(email: email)
+    User.find_by!(email: email)
   end
 end
